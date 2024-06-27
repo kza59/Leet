84d535657bde29a5e59c9560007f4ecfa531b514
Khan <123783741+kza59@users.noreply.github.com>
2024-06-27 09:04:43 -0700

    Time: 5 ms (59.57%), Space: 7.1 MB (33.87%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,6 +4,9 @@ public:
         if(n == 0) return 0;
         if(n == 1) return 1;
         if(n == 2) return 1;
+        if(n == 3) return 2;
+        if(n == 4) return 3;
+        if(n == 5) return 5;
         return fib(n-1) + fib(n-2);
     }
 };
