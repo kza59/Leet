3d54fa6d25cddba82922f3297c188c3d6273df88
Khan <123783741+kza59@users.noreply.github.com>
2024-06-27 09:08:42 -0700

    Time: 1497 ms (23.05%), Space: 7.2 MB (53.67%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -7,6 +7,7 @@ public:
         if(n == 3) return 2;
         if(n == 4) return 4;
         if(n == 5) return 7;
+        if(n == 6) return 13;
         return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
         
     }
